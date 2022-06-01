aad52617fd1f5cdd9ec4d65ea71a37f9a678881e
Chmokachka <karkarech7@gmail.com>
2022-06-01 20:55:52 +0300

    some changes

--- user-data2.sh
+++ user-data2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt update
+sudo apt install ansible -y
+git clone https://github.com/Chmokachka/Geocit134.git
 sudo echo "export DB_USER=${db_username}" >> /etc/environment
 sudo echo "export DB_PASS=${db_password}" >> /etc/environment
 sudo echo "export DB_URL=${db_url}" >> /etc/environment
